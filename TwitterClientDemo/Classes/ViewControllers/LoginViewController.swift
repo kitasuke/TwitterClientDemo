a36473b8ecae18f2ada7c03f405683d3d3d503a5
kitasuke <yusuke2759@gmail.com>
2015-05-20 12:08:22 +0900

    Add LoginViewController and login storyboard

---
+++ TwitterClientDemo/Classes/ViewControllers/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  TwitterClientDemo
+//
+//  Created by Yusuke Kita on 5/20/15.
+//  Copyright (c) 2015 kitasuke. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}

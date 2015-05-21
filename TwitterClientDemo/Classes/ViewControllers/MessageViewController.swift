1f4b33d3c99894efb8bcd81343e67e5216464d9e
kitasuke <yusuke2759@gmail.com>
2015-05-21 13:42:07 +0900

    Support postingView animation with keyboard

--- TwitterClientDemo/Classes/ViewControllers/MessageViewController.swift
+++ TwitterClientDemo/Classes/ViewControllers/MessageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MessageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView?
     @IBOutlet weak var postingView: PostingView?
+    @IBOutlet weak var postingViewBottomConstraint: NSLayoutConstraint?
     
     // MARK: - Lifecycle
     
@@ -19,6 +20,13 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
         
         self.setupNavigationBar()
         self.setupPostingView()
+        self.addKeyboardObserver()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        self.removeKeyboardObserver()
     }
     
     // MARK: - UITableView
@@ -40,6 +48,31 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
     internal func postComment(recognizer: UITapGestureRecognizer) {
     }
     
+    // MARK: - NSNotificationCenter
+    
+    internal func keyboardWillShow(notification: NSNotification) {
+        let userInfo = notification.userInfo as! [String: AnyObject]
+        if let startFrame = userInfo[UIKeyboardFrameBeginUserInfoKey]?.CGRectValue(), let endFrame = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue() {
+            
+            let keyboardHeight: CGFloat
+            if CGRectGetMinY(startFrame) < CGRectGetMaxY(self.view.frame) {
+                keyboardHeight = CGRectGetHeight(endFrame) - CGRectGetHeight(startFrame)
+            } else {
+                keyboardHeight = CGRectGetHeight(startFrame)
+            }
+            self.postingViewBottomConstraint?.constant += keyboardHeight
+            
+            self.animateKeyboard(userInfo)
+        }
+    }
+    
+    internal func keyboardWillHide(notification: NSNotification) {
+        let userInfo = notification.userInfo as! [String: AnyObject]
+        self.postingViewBottomConstraint?.constant = 0
+        
+        self.animateKeyboard(userInfo)
+    }
+    
     // MARK: - Setup
     
     private func setupNavigationBar() {
@@ -51,5 +84,37 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
     
     private func setupPostingView() {
         postingView?.postButton?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("postComment:")))
+    }
+    
+    private func addKeyboardObserver() {
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: Selector("keyboardWillShow:"),
+            name: UIKeyboardWillShowNotification,
+            object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: Selector("keyboardWillHide:"),
+            name: UIKeyboardWillHideNotification,
+            object: nil)
+    }
+    
+    private func removeKeyboardObserver() {
+        NSNotificationCenter.defaultCenter().removeObserver(self,
+            name: UIKeyboardWillShowNotification,
+            object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self,
+            name: UIKeyboardWillHideNotification,
+            object: nil)
+    }
+    
+    // MARK: - Animation operator
+    
+    private func animateKeyboard(userInfo: [String: AnyObject]) {
+        UIView.animateWithDuration((userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue,
+            delay: 0,
+            options: UIViewAnimationOptions(UInt((userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).integerValue << 16)),
+            animations: { [unowned self] () -> Void in
+                self.view.setNeedsLayout()
+                self.view.layoutIfNeeded()
+            }, completion: nil)
     }
 }
